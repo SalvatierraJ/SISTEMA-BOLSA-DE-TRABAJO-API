80e6cdcbdf25629bbb3f7b70fdc9f91c08b6e52c
Lucas <lucadleigue@gmail.com>
2025-05-06 01:11:43 -0400

    ya muestra telefono

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -70,7 +70,7 @@ class authController extends Controller
 
     public function getUser()
     {
-        $user = Auth::user()->load(['rol' , 'personas.estudiantes.carreras']);
+        $user = Auth::user()->load(['rol' , 'personas.estudiantes.carreras' , 'personas.telefonos']);
         return response()->json(compact('user'));
     }
 
