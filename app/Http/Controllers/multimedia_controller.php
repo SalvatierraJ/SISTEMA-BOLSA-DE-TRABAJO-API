bd837d272f6beb9765e1980fc98d735887bb8d34
Javier Salvatierra <javiersalvatierra44@gmail.com>
2025-05-15 09:58:56 -0400

    se corrigio la importacion de un modelo que no coincidia con el nombre

--- app/Http/Controllers/multimedia_controller.php
+++ app/Http/Controllers/multimedia_controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\multimedia;
+use App\Models\Multimedia;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
